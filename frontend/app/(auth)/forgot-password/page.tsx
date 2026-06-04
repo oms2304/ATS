ae9dd35ece6bb4e15829a25f44ba2a6bc61ca184
Roaa Elsayed <rme9@njit.edu>
2026-06-04 19:21:38 -0400

    fix forgot-password page to resolve typescript module error

--- frontend/app/(auth)/forgot-password/page.tsx
+++ frontend/app/(auth)/forgot-password/page.tsx
@@ -1,10 +1,4 @@
-export default function ForgotPasswordPage() {
-  return (
-    <div>
-      <h1>Forgot Password</h1>
-    </div>
-  );
-}export default function ForgotPasswordPage() {
+﻿export default function ForgotPasswordPage() {
   return (
     <div>
       <h1>Forgot Password</h1>
